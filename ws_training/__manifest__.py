85ed74ae81b3bf693e349c50ecd14abdc1b570c7
Your Name <you@example.com>
2018-12-07 11:27:40 +0000

    4444

---
+++ ws_training/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PS Cloud 培训',
+    'version': '12.0.1.0',
+    'summary': 'PS Cloud 培训',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+         'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'views/training_lesson_views.xml',
+        'views/training_subject_views.xml',
+        'views/training_views.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
